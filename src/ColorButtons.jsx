c03ff01a83f8c5dec9d369efd8ca641605047f14
danx <rushn24@gmail.com>
2024-07-12 13:17:33 -0400

    broke down further into individual ColorButton component

--- src/ColorButtons.jsx
+++ src/ColorButtons.jsx
@@ -1,21 +1,14 @@
-import React, { useState } from 'react';
-import './ColorButtons.css';
+import React from "react";
+import ColorButton from "./ColorButton";
 
-const ColorButtons = ({ addCircle, options}) => {
-    return (
-        <div className="ColorButtons">
-            {options.map(color => (
-                <button 
-                    onClick={() => addCircle(color)}
-                    className="ColorButtons-btn"
-                    style={{ backgroundColor: color }}
-                    key={color}
-                >
-                    {color}
-                </button>
-             ))}
-        </div>
-    )
-}
+const ColorButtons = ({ addCircle, options }) => {
+  return (
+    <div className="ColorButtons">
+      {options.map((color) => (
+        <ColorButton color={color} addCircle={addCircle} key={color} />
+      ))}
+    </div>
+  );
+};
 
 export default ColorButtons;
